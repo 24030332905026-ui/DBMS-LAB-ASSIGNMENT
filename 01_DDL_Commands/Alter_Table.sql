b375ed07724557fbbebe4ef1dfafb0ba4b9972d6
24030332905026-ui <24030332905026@dbatu.ac.in>
2026-06-06 14:31:12 +0530

    Update Alter_Table.sql

--- 01_DDL_Commands/Alter_Table.sql
+++ 01_DDL_Commands/Alter_Table.sql
@@ -1,4 +1,32 @@
 
-CREATE DATABASE CollegeDB;
-SHOW DATABASES;
-USE CollegeDB;
+-- Create Table
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    Course VARCHAR(50)
+);
+
+-- Add a new column
+ALTER TABLE Student
+ADD Email VARCHAR(100);
+
+-- Add multiple columns
+ALTER TABLE Student
+ADD Address VARCHAR(100),
+ADD Phone VARCHAR(15);
+
+-- Modify a column
+ALTER TABLE Student
+MODIFY Name VARCHAR(100);
+
+-- Rename a column
+ALTER TABLE Student
+RENAME COLUMN Course TO Department;
+
+-- Drop a column
+ALTER TABLE Student
+DROP COLUMN Phone;
+
+-- View table structure
+DESC Student;
