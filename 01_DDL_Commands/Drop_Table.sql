550eaa12395d01410ae9ab79efed2b3511c8b6bc
24030332905026-ui <24030332905026@dbatu.ac.in>
2026-06-06 14:36:38 +0530

    Create Drop_Table.sql

---
+++ 01_DDL_Commands/Drop_Table.sql
@@ -0,0 +1,15 @@
+-- Create Table
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT
+);
+
+-- Display Tables
+SHOW TABLES;
+
+-- Drop Table
+DROP TABLE Student;
+
+-- Display Tables Again
+SHOW TABLES;
